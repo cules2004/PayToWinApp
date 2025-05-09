3bb7f4abdfdea4b3538bb937d34b1c94abf67f1b
cules2004 <duydan257@gmail.com>
2025-05-09 15:40:44 +0700

    Gradient Title

---
+++ src/screens/Gradient.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Text, View } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import MaskedView from "@react-native-masked-view/masked-view";
+
+export function GradientText(props){
+    return (
+        <MaskedView maskElement={<Text style = {[props.styles, {backgroundColor: 'transparent'}]}>{props.text}</Text>}>
+            <LinearGradient
+                start={{x:0, y:0}}
+                end={{x:1, y:1}}
+                colors={['#14E585','#9E01B7']}
+            >
+                <Text style={[props.styles,{opacity:0},{backgroundColor:'transparent'}]}>{props.text}</Text> 
+            </LinearGradient>
+        </MaskedView>
+    );
+}
--- src/screens/SplashScreen.js
+++ src/screens/SplashScreen.js
@@ -1,25 +1,55 @@
 // screens/SplashScreen.js
-import React from 'react';
+import React, {useEffect} from 'react';
 import { View, Text, StyleSheet, ActivityIndicator, Image } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
+import { GradientText } from './Gradient'; 
 
-const SplashScreen = () => {
+// const SplashScreen = ({ navigation }) => {
+//   const title = "PayToWin"
+//   // useEffect(() => {
+//   //   const timer = setTimeout(() => {
+//   //     navigation.replace('Splash2'); // Chuyển sang SplashScreen2
+//   //   }, 3000);
+//   //   return () => clearTimeout(timer); // Dọn dẹp timer
+//   // }, [navigation]);
+//   return (
+//     <LinearGradient
+//       colors={['#0A0C2B', '#2A2D5D']}
+//       style={styles.container}
+//     >
+//       <Text style={styles.title}>{title}</Text>
+//     {/* <GradientText text = {title} style = {styles.title} /> */}
+//       <ActivityIndicator size="large" color="#fff" style={styles.spinner} />
+//     </LinearGradient>
+//   );
+// };
+const SplashScreen = ({ navigation }) => {
   return (
-    <LinearGradient
-      colors={['#0A0C2B', '#2A2D5D']}
+    <View
       style={styles.container}
     >
-      <Text style={styles.title}>PayToWin</Text>
+      <GradientText text = "PayToWin" style = {{fontSize: 48}} />
+      {/* <Text style={styles.title}>{title}</Text> */}
+    {/* <GradientText text = {title} style = {styles.title} /> */}
       <ActivityIndicator size="large" color="#fff" style={styles.spinner} />
-    </LinearGradient>
+    </View>
   );
 };
+// const SplashScreen2 = ({ navigation }) => {
+//   return (
+//     <View style = {styles.container}>
+//       <GradientText text = "PayToWin" style = {styles.title} />
+//       <ActivityIndicator size="large" color="#fff" style={styles.spinner} />
+//     </View>
+//   );
+// };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor:'#0A0C2B'
   },
   title: {
     fontSize: 48,
